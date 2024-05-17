9e7318559b5e79dff6872881eecf2c2a7ffcbc68
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-05-17 15:12:21 +0300

    Initial Commit

---
+++ DismissModalAfterSeveralScreens/DismissModalAfterSeveralScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  DismissModalAfterSeveralScreensApp.swift
+//  DismissModalAfterSeveralScreens
+//
+//  Created by Gene Bogdanovich on 17.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DismissModalAfterSeveralScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
